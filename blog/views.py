11a570030fca087fff5c367d80874fda0afb76a4
Lkad <eloony@163.com>
2017-04-24 20:03:12 +0800

    new file:   blog/urls.py 	modified:   mysite/settings.py 	modified:   mysite/urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request,'blog/index.html',context={'title':'my index bolg','welcom':'welcom to my blog'})
 
 # Create your views here.
