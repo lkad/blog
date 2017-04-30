618d5b4bcf4191ecf4729b24c0347e9f130d778a
root <root@gitlab.jty>
2017-04-30 21:05:41 +0800

    modified:   blog/admin.py 	modified:   blog/migrations/0001_initial.py 	modified:   blog/models.py 	modified:   blog/urls.py 	modified:   blog/views.py
    	blog/static/
    	mysite/__pycache__/
    	templates/blog/index.html.bak

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.six import python_2_unicode_compatible
+from django.urls import reverse
 
 # Create your models here.
 @python_2_unicode_compatible
@@ -27,4 +28,5 @@ class Post(models.Model):
     author = models.ForeignKey(User)
     def __str__(self):
         return self.title
-
+    def get_absolute_url(self):
+        return reverse('blog:detail',kwargs={'pk':self.pk})
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    return render(request,'blog/index.html',context={'title':'my index bolg','welcom':'welcom to my blog'})
+    post_list = Post.objects.all()
+    return render(request,'blog/index.html',context={'post_list':post_list})
 
 # Create your views here.
+def detal(request,pk):
+    post = get_obiect_or_404(Post,pk=pk)
+    return render(request,'blog/detail.html',context={'post':post})
